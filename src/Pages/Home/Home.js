d7b3ea90fb36dd620ffe66a0095a0f3fe5fd5f92
Irina <irinasverzhanovskaya69@gmail.com>
2023-05-05 19:38:22 -0400

    home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,12 +7,12 @@ import { Link } from 'react-router-dom';
 export const HomePage = () => {
     const [name, setName] = useState('');
     const [color, setColor] = useState('');
+
     const [inputColor, setInputColor] = useState('');
     const [showPicker, setShowPicker] = useState(true);
 
     useEffect(() => {
         const savedName = localStorage.getItem("name");
-        console.log('saveName')
         if (savedName) {
             console.log('savedName')
             setName(savedName);
@@ -51,23 +51,37 @@ export const HomePage = () => {
     };
      
 return (
-    <div className={styles.homePage}>
-            <img src={logo} alt="logo" className={styles.logo} />
-          <div className={styles.container}>
-            <div className={styles.nameInputContainer}>
-              <label className={styles.nameInputLabel}>Hello. What's your name?</label>
-                 <input
-                      type="text"
-                      className={styles.nameInput}
-                      onChange={handleNameChange}
-                      placeholder="my name is..."
-                   />  
-                     <div className={styles.separateLine}></div> 
+  <div className={styles.homePage}>
+    <div className={styles.logoContainer}>
+      <img src={logo} alt="logo" className={styles.logo} />
+    </div>
+      <div className={styles.container}>
+        <div className={styles.nameInputContainer}>
+        <label className={styles.nameInputLabel}>Hello. What's your name?</label>
+         <input
+              type="text"
+              className={styles.nameInput}
+              onChange={handleNameChange}
+              placeholder="my name is..."
+          />  
+            <div className={styles.separateLine}></div> 
           </div>
-          <div className={styles.colorInputContainer}>
-          <div className={styles.colorLabelContainer}>
-          <div className={styles.favoriteColor}>What's your favorite <span style={{ color: inputColor }}>color </span>?</div>
+            <div className={styles.colorInputContainer}>
+            <div className={styles.colorLabelContainer}>
+          <div className={styles.favoriteColor}>What's your favorite <span style={{ color: inputColor }}>color</span>?
+            <div style={{  backgroundColor: color, height: '52px', marginLeft:'100px', marginTop:'30px', width: '52px', borderRadius: '8px' }}>
+            <div className={styles.colorContainer}>
+           <input 
+                className={styles.colorText}
+                type="text" 
+                value={color} 
+                onChange={handleColorChange} 
+            />
+            <div className={styles.separateLineTwo}></div> 
+            </div>
           </div>
+        </div>
+      </div>
           <div className={styles.colorPickerContainer}>
           <div className={styles.colorBox} style={{ backgroundColor: color }} onClick={togglePicker} />
           {showPicker && (
@@ -77,16 +91,9 @@ return (
                          onChange={handleColorChange} 
                   />
             )}
-        <div style={{ backgroundColor: color, height: '52px', width: '52px', position:"absolute", left:'220px', top:'490px', borderRadius: '8px' }}></div>
-           <input 
-                className={styles.colorText}
-                type="text" 
-                value={color} 
-                onChange={handleColorChange} 
-            />
-            <div className={styles.separateLineTwo}></div> 
+            </div>
+          
             <Link to="/landing-page" className={styles.finish}>Finish</Link>
-          </div>
         </div>
     </div>
 </div>
